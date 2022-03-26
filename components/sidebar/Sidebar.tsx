631e7948c6b7e6eea585b182272e746d2126aceb
William Owen <william.owen.bsc@gmail.com>
2022-03-26 16:14:24 -0700

    got the dark mode trigger.. now need to change styles

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import logo from "./assets/logo.svg";
 import moon from "./assets/icon-moon.svg";
 import sun from "./assets/icon-sun.svg";
@@ -6,14 +6,27 @@ import avatar from "./assets/image-avatar.jpg";
 import style from "./Sidebar.module.scss";
 
 export default function Sidebar(props) {
-    const dark = true;
+    const [dark, setDark] = useState(false);
+
+    useEffect(() => {
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            setDark(true);
+            toggleDarkMode();
+        }
+    }, []);
+
+    const toggleDarkMode = () => {
+        const el = document.body;
+        el.classList.toggle("dark-mode");
+        setDark(!dark);
+    };
 
     return (
         <div className={style.sidebar}>
             <div className={style.logo}>{logo()}</div>
 
             <div className={style.bottom}>
-                <div className={style.theme}>
+                <div className={style.theme} onClick={toggleDarkMode}>
                     {!dark && moon()}
                     {dark && sun()}
                 </div>
