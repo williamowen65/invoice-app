6114b7cdb214d9068607e6d3e3809368284c5c9d
William Owen <william.owen.bsc@gmail.com>
2022-03-27 09:01:00 -0700

    added new String method.... 😎

--- components/ticket/Ticket.tsx
+++ components/ticket/Ticket.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import style from "./Ticket.module.scss";
 import arrow from "./assets/icon-arrow-right.svg";
+import { stringify } from "querystring";
 
 export default function Ticket({ data }) {
     const { id, paymentDue, clientName, total, status } = data;
+
+    String.prototype.capitialize = function () {
+        const first = this[0].toUpperCase();
+        const string = first + this.slice(1);
+        return string;
+    };
+
     return (
         <div className={style.ticket + " ticket"}>
             <span className={style.id}>
@@ -15,7 +23,9 @@ export default function Ticket({ data }) {
             </span>
             <span className={style.client + " ticket-off-2"}>{clientName}</span>
             <span className={style.total}>£{total}</span>
-            <span className={style.status + " " + style[status]}>{status}</span>
+            <span className={style.status + " " + style[status] + " " + status}>
+                {status.capitialize()}
+            </span>
             <span className={style.arrow}>{arrow()}</span>
         </div>
     );
