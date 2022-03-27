a822785488d5949925b1478d5623b50d3eaa29d1
William Owen <william.owen.bsc@gmail.com>
2022-03-26 22:10:34 -0700

    card work

--- components/ticket/Ticket.tsx
+++ components/ticket/Ticket.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import style from "./Ticket.module.scss";
+import arrow from "./assets/icon-arrow-right.svg";
 
 export default function Ticket({ data }) {
     const { id, paymentDue, clientName, total, status } = data;
     return (
         <div className={style.ticket}>
-            <span>
+            <span className={style.id}>
                 <span className='ticket-off-1'>#</span>
                 {id}
             </span>
-            <span className='ticket-off-1'>Due {paymentDue}</span>
-            <span className='ticket-off-2'>{clientName}</span>
-            <span>£{total}</span>
-            <span>{status}</span>
+            <span className={style.due + " ticket-off-1"}>
+                Due {paymentDue}
+            </span>
+            <span className={style.client + " ticket-off-2"}>{clientName}</span>
+            <span className={style.total}>£{total}</span>
+            <span className={style.status}>{status}</span>
+            <span className={style.arrow}>{arrow()}</span>
         </div>
     );
 }
