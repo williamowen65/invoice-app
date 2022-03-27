e65a2e2bcdbd7a828b7267adfffa9b4d9e2aadd5
William Owen <william.owen.bsc@gmail.com>
2022-03-27 15:14:09 -0700

    invoice styles

--- pages/invoice/[id].tsx
+++ pages/invoice/[id].tsx
@@ -37,45 +37,45 @@ export default function Invoice({ data }) {
             <>
                 <InvoiceHeader data={invoice} />
                 <div className={style.invoice + " invoice"}>
-                    <div className='row'>
-                        <div>
+                    <div className={style.row}>
+                        <div className={style.list}>
                             <span>#{invoice.id}</span>
                             <span>{invoice.description}</span>
                         </div>
-                        <div>
+                        <div className={style.list}>
                             <span>{invoice.senderAddress.street}</span>
                             <span>{invoice.senderAddress.city}</span>
                             <span>{invoice.senderAddress.postCode}</span>
                             <span>{invoice.senderAddress.country}</span>
                         </div>
                     </div>
-                    <div className='row'>
-                        <div className='dates'>
+                    <div className={style.row}>
+                        <div className={style.list + " dates"}>
                             <span className='createdAt'>
                                 <span>Invoice Date</span>
                                 <span>{invoice.createdAt}</span>
                             </span>
-                            <span className='due'>
+                            <span className={style.list + " due"}>
                                 <span>Payment Due</span>
                                 <span>{invoice.paymentDue}</span>
                             </span>
                         </div>
-                        <div className='billTo'>
+                        <div className={style.list + " billTo"}>
                             <span className='client'>{invoice.clientName}</span>
-                            <span className='address'>
+                            <span className={style.list + " address"}>
                                 <span>{invoice.clientAddress.street}</span>
                                 <span>{invoice.clientAddress.city}</span>
                                 <span>{invoice.clientAddress.postCode}</span>
                                 <span>{invoice.clientAddress.country}</span>
                             </span>
                         </div>
-                        <div className='sentTo'>
+                        <div className={style.list + " sentTo"}>
                             <span>Sent to</span>
                             <span>{invoice.clientEmail}</span>
                         </div>
                     </div>
-                    <div className='card'>
-                        <div className='items'>
+                    <div className={style.invoiceSummary + " invoiceSummary"}>
+                        <div className={style.items + " items"}>
                             <table>
                                 <tr>
                                     <th>Item Name</th>
@@ -86,7 +86,7 @@ export default function Invoice({ data }) {
                                 {items}
                             </table>
                         </div>
-                        <div className='summary'>
+                        <div className={style.summary + " summary"}>
                             <span>Amount Due</span>
                             <span>£ {invoice.total}</span>
                         </div>
